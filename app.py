ca962e464c21724f5282b71a88aa0234600528d0
vijitdesilva <vijit.desilva@gmail.com>
2021-04-19 00:48:59 -0400

    html updated

--- app.py
+++ app.py
@@ -9,23 +9,22 @@ Created on Fri Apr 16 21:18:53 2021
 # Dependencies and Setup
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import scrape_mars
+import scrape3_mars
 
-#################################################
+
 # Flask Setup
-#################################################
+
 app = Flask(__name__)
 
-#################################################
+
 # PyMongo Connection Setup
-#################################################
+
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-#################################################
+
 # Flask Routes
-#################################################
-# Root Route to Query MongoDB & Pass Mars Data Into HTML Template: index.html to Display Data
+
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
@@ -35,7 +34,7 @@ def index():
 @app.route("/scrape")
 def scrapper():
     mars = mongo.db.mars
-    mars_data = scrape_mars.scrape_all()
+    mars_data = scrape3_mars.scrape_all()
     mars.update({}, mars_data, upsert=True)
     return "Scraping Successful"
 
